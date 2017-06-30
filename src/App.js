acacd7fc3e679a63d7a438db48fc1dce9dcbf7ce
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-30 18:05:49 -0400

    Add in CSS for background

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="title">
+          <span>Welcome to React</span>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
