eeda74985346f4fb617545b877df2e774c4effd2
Andrew Arpasi <andrew@andrewarpasi.com>
2017-08-11 21:59:49 -0400

    Change background styles and add gradient button component

---
+++ src/GradientButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GradientButton extends Component {
+  render() {
+    return (
+      <div className="gradientButton">
+        
+      </div>
+    );
+  }
+}
+
+export default GradientButton
