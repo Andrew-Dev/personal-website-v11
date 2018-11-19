414a108e2f902d42889a8ef2d6133f2b2e9d5b1f
Andrew Arpasi <andrew@andrewarpasi.com>
2018-11-19 15:01:39 -0500

    I am now a junior, not a sophomore

--- src/Introduction.js
+++ src/Introduction.js
@@ -6,7 +6,7 @@ class Introduction extends Component {
       <div id="introduction">
         <h1 className="lead introLead">About</h1>
         <img src="/img/me.png"/>
-        <p id="introBody">I am a sophomore at Purdue University studying Computer Science. I am always striving to learn new technologies, and I aspire to use my knowledge in a way that is beneficial to others. I have worked on many different projects, including full stack web applications and mobile applications. I am also currently a software developer for the Purdue Cognition and Learning Lab.</p>
+        <p id="introBody">I am a junior at Purdue University studying Computer Science. I am always striving to learn new technologies, and I aspire to use my knowledge in a way that is beneficial to others. I have worked on many different projects, including full stack web applications and mobile applications. I am also currently a software developer for the Purdue Cognition and Learning Lab.</p>
       </div>
     )
   }
